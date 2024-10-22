99717a88acc5ea0e2b4aa964b666eed2bbb1ee66
Harry Richardson <richardson.chesney@gmail.com>
2024-10-22 06:27:26 -0500

    more movement

--- tofu/bootstrap/providers.tf
+++ tofu/bootstrap/providers.tf
@@ -3,29 +3,31 @@ provider "digitalocean" {
 }
 
 provider "kubernetes" {
-  host  = digitalocean_kubernetes_cluster.web-public.endpoint
-  token = digitalocean_kubernetes_cluster.web-public.kube_config[0].token
-  cluster_ca_certificate = base64decode(
-    digitalocean_kubernetes_cluster.web-public.kube_config[0].cluster_ca_certificate
-  )
+  #  host  = digitalocean_kubernetes_cluster.web-public.endpoint
+  #  token = digitalocean_kubernetes_cluster.web-public.kube_config[0].token
+  #  cluster_ca_certificate = base64decode(
+  #    digitalocean_kubernetes_cluster.web-public.kube_config[0].cluster_ca_certificate
+  #  )
+  config_path = "~/.kube/config"
 }
 
 provider "helm" {
   kubernetes {
     #    host  = digitalocean_kubernetes_cluster.web-public.endpoint
-    host  = digitalocean_kubernetes_cluster.web-public.endpoint
-    token = digitalocean_kubernetes_cluster.web-public.kube_config[0].token
-    cluster_ca_certificate = base64decode(
-      digitalocean_kubernetes_cluster.web-public.kube_config[0].cluster_ca_certificate
-    )
+    #    host  = digitalocean_kubernetes_cluster.web-public.endpoint
+    #    token = digitalocean_kubernetes_cluster.web-public.kube_config[0].token
+    #    cluster_ca_certificate = base64decode(
+    #      digitalocean_kubernetes_cluster.web-public.kube_config[0].cluster_ca_certificate
+    #    )
+    config_path = "~/.kube/config"
   }
 }
 
 provider "kubectl" {
-  host  = digitalocean_kubernetes_cluster.web-public.endpoint
-  token = digitalocean_kubernetes_cluster.web-public.kube_config[0].token
-  cluster_ca_certificate = base64decode(
-    digitalocean_kubernetes_cluster.web-public.kube_config[0].cluster_ca_certificate
-  )
-  load_config_file = false
+  #  host  = digitalocean_kubernetes_cluster.web-public.endpoint
+  #  token = digitalocean_kubernetes_cluster.web-public.kube_config[0].token
+  #  cluster_ca_certificate = base64decode(
+  #    digitalocean_kubernetes_cluster.web-public.kube_config[0].cluster_ca_certificate
+  #  )
+  load_config_file = true
 }
